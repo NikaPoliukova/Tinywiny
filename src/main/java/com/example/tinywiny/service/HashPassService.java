61f2b072f8b50282c8d6636755185bfa1fde21b5
NikaPoliukova <nikanarchuk@gmail.com>
2023-03-20 22:55:00 +0400

    add real image on list products page

--- src/main/java/com/example/tinywiny/service/HashPassService.java
+++ src/main/java/com/example/tinywiny/service/HashPassService.java
@@ -1,6 +1,7 @@
 package com.example.tinywiny.service;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
@@ -10,8 +11,11 @@ public class HashPassService {
 
   private final BCrypt.Hasher hasher;
 
+  @Value("${security.bcrypt-secret}")
+  private String secretKey;
+
   public HashPassService() {
-    hasher = BCrypt.with(new SecureRandom("secret".getBytes()));
+    hasher = BCrypt.with(new SecureRandom(secretKey.getBytes()));
   }
 
   public boolean verify(String password, String hashPass) {
